026db9bdbd8d96279c1f831f59f0779681126e3b
Ho-Ren <horenkan50@gmail.com>
2017-10-29 14:11:18 -0700

    add exception

--- Server.py
+++ Server.py
@@ -5,11 +5,25 @@ serverSocket = socket (AF_INET, SOCK_STREAM)
 serverSocket.bind(('localhost', 90))
 serverSocket.listen(5)
 
-while true:
+while True:
     #Establish the connection
     print ("Ready to serve..")
     connectionSocket, address = serverSocket.accept()
 
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:], "rb")
+        outputdata = f.read()
+        #send one http header line into socket
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i:i+1])
+        connectionSocket.send(b'\r\n\r\n')
+        connectionSocket.close()
+    except IOError:
+        #file not found
+        connectionSocket.close()
 
 
 
