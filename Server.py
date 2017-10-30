657252303d0f605465bf6595f325068f676ae20d
Ho-Ren <horenkan50@gmail.com>
2017-10-30 14:55:35 -0700

    Converted string to byte

--- Server.py
+++ Server.py
@@ -1,8 +1,7 @@
-import socket
 from socket import *
 serverSocket = socket (AF_INET, SOCK_STREAM)
 #prepare a server socket
-serverSocket.bind(('localhost', 90))
+serverSocket.bind(('localhost', 80))
 serverSocket.listen(5)
 
 while True:
@@ -15,15 +14,19 @@ while True:
         filename = message.split()[1]
         f = open(filename[1:], "rb")
         outputdata = f.read()
+        print ("After reading")
         #send one http header line into socket
-        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
+        connectionSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "UTF-8"))
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i:i+1])
         connectionSocket.send(b'\r\n\r\n')
         connectionSocket.close()
     except IOError:
         #file not found
+        print("file not found")
         connectionSocket.close()
+
+serverSocket.close()
 
 
 
